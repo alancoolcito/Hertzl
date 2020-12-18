dd3d49b201a6e39ad68e6cfe2bb98973c7946e7d
alancoolcito <alaneroperel@gmail.com>
2020-12-18 18:39:53 +0200

    Finished Alpha Mechanism!

---
+++ Assets/Hertzl/Scripts/DestoryReporters.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryReporters : MonoBehaviour
+{
+    public GameObject Reporter1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    public void KillThemAll()
+    {
+        Destroy(Reporter1);
+    }
+}
